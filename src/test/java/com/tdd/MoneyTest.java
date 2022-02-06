7a0f530dcbaa81f865dcccedfe4a984e0b34c2a1
dev.og <dev.og.eun@gmail.com>
2022-02-06 17:51:26 +0900

    test 02. 실패 Expected: 15 Actual: 30

--- src/test/java/com/tdd/MoneyTest.java
+++ src/test/java/com/tdd/MoneyTest.java
@@ -8,8 +8,10 @@ public class MoneyTest {
     // 결과: 컴파일 에러(Dollar 클래스 없음, 생성자 없음, times(int) 메소드 없음, amount 필드 없음)
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar dollar = new Dollar(5);
+        dollar.times(2);
+        assertEquals(10, dollar.amount);
+        dollar.times(3);
+        assertEquals(15, dollar.amount);
     }
 }
