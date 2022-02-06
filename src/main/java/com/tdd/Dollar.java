c054ec53ecf62ab55cb46c02a3dea0a251c3de5c
dev.og <dev.og.eun@gmail.com>
2022-02-06 17:48:26 +0900

    source code 02. 테스트가 성공할 수 있도록 수정

--- src/main/java/com/tdd/Dollar.java
+++ src/main/java/com/tdd/Dollar.java
@@ -2,13 +2,13 @@ package com.tdd;
 
 public class Dollar {
 
-    public int amount;
+    public int amount = 10;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
-
+        amount *= multiplier;
     }
 }
