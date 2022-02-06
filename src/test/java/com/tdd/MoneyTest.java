3c2877793c4fe7f617b2f13c3de41bd07b1f2971
dev.og <dev.og.eun@gmail.com>
2022-02-06 17:39:10 +0900

    환율 적용 미니: 테스트 코드 01 - 곱셈 (주석 추가)

--- src/test/java/com/tdd/MoneyTest.java
+++ src/test/java/com/tdd/MoneyTest.java
@@ -3,7 +3,8 @@ package com.tdd;
 import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
-    // 곱셈
+    // Test 01. 곱셈 테스트
+    // 결과: 컴파일 에러(Dollar 클래스 없음, 생성자 없음, times(int) 메소드 없음, amount 필드 없음)
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
