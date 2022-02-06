82f1210a23ec6d260367103467fe333a629996e1
dev.og <dev.og.eun@gmail.com>
2022-02-06 17:56:55 +0900

    test 03. 성공

--- src/test/java/com/tdd/MoneyTest.java
+++ src/test/java/com/tdd/MoneyTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
-    // Test 02. 곱셈 테스트
-    // 결과: times 메소드 수행 시 Dollar 의 amount 값이 변경됨
+    // Test 03. 곱셈 테스트
+    // 결과: 성공
     @Test
     public void testMultiplication() {
         Dollar dollar = new Dollar(5);
-        dollar.times(2);
-        assertEquals(10, dollar.amount);
-        dollar.times(3);
-        assertEquals(15, dollar.amount);
+        assertEquals(10, dollar.times(2));
+        assertEquals(15, dollar.times(3));
     }
 }
