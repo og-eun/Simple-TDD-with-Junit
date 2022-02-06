9b2941edbd02915e0b34cf1383cfaae157374a28
dev.og <dev.og.eun@gmail.com>
2022-02-06 17:52:10 +0900

    test 02. 실패 주석 추가

--- src/test/java/com/tdd/MoneyTest.java
+++ src/test/java/com/tdd/MoneyTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
-    // Test 01. 곱셈 테스트
-    // 결과: 컴파일 에러(Dollar 클래스 없음, 생성자 없음, times(int) 메소드 없음, amount 필드 없음)
+    // Test 02. 곱셈 테스트
+    // 결과: times 메소드 수행 시 Dollar 의 amount 값이 변경됨
     @Test
     public void testMultiplication() {
         Dollar dollar = new Dollar(5);
